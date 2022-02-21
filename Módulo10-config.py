f83802fbcb7090df0d4fc957c4122e83a253db5d
HectordAponte <99517955+HectordAponte@users.noreply.github.com>
2022-02-21 17:36:43 -0600

    Scripts Python del Módulo 10

--- Módulo10-config.py
+++ Módulo10-config.py
@@ -1,9 +1,29 @@
+"""
 def main():
     try:
-        configuration = open('config.txt')
+        configuration = open('config2.txt')
     except FileNotFoundError:
         print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except PermissionError:
+        print("You don't have permissions to open this file")
 
+if __name__ == '__main__':
+    main()
+"""
+
+def main():
+    try:
+        configuration = open('config2.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except PermissionError:
+        print("You don't have permissions to open this file")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
 
 if __name__ == '__main__':
     main()
---
+++ Módulo10-water_left.py
@@ -0,0 +1,36 @@
+'''
+# Primera versión
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+# Con detección de errores:
+
+def water_left(astronauts, water_left, days_left):
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+'''
+# Con filtración de tipos incorrectos de entrada
+
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            # If argument is an int, the following operation will work
+            argument / 10
+        except TypeError:
+            # TypError will be raised only if it isn't the right type 
+            # Raise the same exception but with a better error message
+            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
